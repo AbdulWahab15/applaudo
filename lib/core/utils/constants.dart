68fce7771f25ea49a6ff5c37715ee260ccd4b158
Abdul Wahab Usmani <awusmani30@gmail.com>
2023-01-26 22:34:59 +0500

    token updated.

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,7 +2,7 @@ class Constants {
   static const aDebugBaseUrl = 'https://api.github.com/search';
   static const searchRepoUrl = '/repositories';
   static const searchRepoIssuesUrl = '/issues';
-  static const token = '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';
+  static const token = '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';
   static const perPage = 100;
   static const searchMaxLength = 4;
 }
